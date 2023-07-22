5aa8baa9d5f25cb2104fd951a199928d1c61ba3f
Robbie Decker <robbd56@gmail.com>
2023-07-21 21:07:56 -0400

    Trying to figure out routing

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite"
+import { resolve } from "path"
 
 // vite.config.js
 export default defineConfig({
     // config options
     base: '/Portfolio/',
-
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, "index.html"),
+            projects: resolve(__dirname, "pages/projects.html"),
+          },
+        },
+      },
   })
